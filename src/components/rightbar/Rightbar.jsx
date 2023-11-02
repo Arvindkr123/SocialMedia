1b20fb19d398be3a8a8830b7e56958d01b246c70
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-11-02 09:34:02 +0530

    added

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,225 @@
 import React from "react";
 import "./Rightbar.scss";
 const Rightbar = () => {
-  return <div>Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="container">
+        {/* ================================== 
+         item 1 starts here  
+        =================================== */}
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <span>Arvind K</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismis</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <span>Arvind K</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismis</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <span>Arvind K</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismis</button>
+            </div>
+          </div>
+        </div>
+
+        {/* ================================== 
+         item 2 starts here  
+        =================================== */}
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <p>
+                <span>Arvind K</span> changed thier cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <p>
+                <span>Arvind K</span> changed thier cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <p>
+                <span>Arvind K</span> changed thier cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <p>
+                <span>Arvind K</span> changed thier cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <p>
+                <span>Arvind K</span> changed thier cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <p>
+                <span>Arvind K</span> changed thier cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <p>
+                <span>Arvind K</span> changed thier cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        {/* ================================== 
+         item 3 starts here  
+        =================================== */}
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <div className="online"></div>
+              <span>Arvind K</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <div className="online"></div>
+              <span>Arvind K</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <div className="online"></div>
+              <span>Arvind K</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <div className="online"></div>
+              <span>Arvind K</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <div className="online"></div>
+              <span>Arvind K</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <div className="online"></div>
+              <span>Arvind K</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user image"
+              />
+              <div className="online"></div>
+              <span>Arvind K</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;

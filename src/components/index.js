6b84eec7c035ac3b2e168b3abfa2364f8ff4f3d9
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-11-05 17:17:30 +0530

    added

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ import Navbar from "./navbar/Navbar";
 import Leftbar from "./leftbar/Leftbar";
 import Rightbar from "./rightbar/Rightbar";
 import Story from "./stories/Story";
-import Post from "./posts/Post";
-export { Navbar, Leftbar, Rightbar, Story, Post };
+import Posts from "./posts/Posts";
+export { Navbar, Leftbar, Rightbar, Story, Posts };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Home.scss";
-import { Post, Story } from "../../components";
+import { Posts, Story } from "../../components";
 const Home = () => {
   return (
     <div className="home">
       <Story />
-      <Post />
+      <Posts />
     </div>
   );
 };
